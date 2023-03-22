0e0adbabf63ed16ce829a15bb490ac05b226104a
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2023-03-22 12:11:10 +0100

    Nou nee

---
+++ js/Jokes.js
@@ -0,0 +1,12 @@
+"use strict"
+export default 
+class joke{
+    constructor(categroy,flaggs,joketype,amount,safe){
+
+        this.categroy = categroy;
+        this.flaggs = flaggs;
+        this.joketype = joketype;
+        this.amount = amount;
+        this.safe = safe;
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,15 +1,40 @@
 "use strict";
+import joke from './Jokes.js';
 
 const app = {
+    
     items: [],
     init() {
+        this.getData();
     },
     getData() {
+        fetch('https://v2.jokeapi.dev/joke/Any')
+        .then(incommingJoke => incommingJoke.json())
+        .then(function(joke){
+            console.log(joke);
+
+            joke.forEach(function(joke) {
+                const jokeHTML = `
+                
+                
+                <div class="item-field">Setup:\n${joke.setup}</div>
+                <div class="item-field">delivery: \n${joke.delivery}</div>
+            
+                    
+            
+          `
+          document.querySelector('.item').insertAdjacentHTML('beforeend',jokeHTML )
+            });
+        })
+        
     },
     onSearch() {
     },
     render(message) {
+
+        
     }
 
 }
+
 app.init()
